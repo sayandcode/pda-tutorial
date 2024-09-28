3d6b5728cbf054e6aaa6537147267553ad1c42c5
Sayand Sathish <sayandcode@gmail.com>
2024-09-28 18:48:17 +0530

    test: Include network fees in `expect` calc's

--- tests/_utils.ts
+++ tests/_utils.ts
@@ -1,10 +1,21 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Pda } from "../target/types/pda";
+import { PublicKey } from "@solana/web3.js";
 
 const defaultProvider = anchor.AnchorProvider.env();
 anchor.setProvider(defaultProvider);
 
-const program = anchor.workspace.pda as anchor.Program<Pda>;
-const wallet = defaultProvider.wallet;
+export const program = anchor.workspace.pda as anchor.Program<Pda>;
+export const wallet = defaultProvider.wallet;
 
-export { wallet, program };
+export async function getBal(pk: PublicKey) {
+  return program.provider.connection.getBalance(pk);
+}
+
+export async function getTxnFees(txnHash: string) {
+  const txnInfo = await program.provider.connection.getTransaction(txnHash, {
+    commitment: "confirmed",
+    maxSupportedTransactionVersion: 0,
+  });
+  return txnInfo.meta.fee;
+}
--- tests/pda-tutorial.ts
+++ tests/pda-tutorial.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { wallet, program } from "./_utils";
+import { wallet, program, getBal, getTxnFees } from "./_utils";
 import { PublicKey } from "@solana/web3.js";
 
 describe("PDA Tutorial", () => {
@@ -25,63 +25,59 @@ describe("PDA Tutorial", () => {
   });
 
   it("Update Message Account", async () => {
-    const vaultBalBefore = await program.provider.connection.getBalance(
-      vaultPda
-    );
+    const vaultBalBefore = await getBal(vaultPda);
     expect(vaultBalBefore).to.equal(0);
-    const userBalBefore = await program.provider.connection.getBalance(
-      wallet.publicKey
-    );
+    const userBalBefore = await getBal(wallet.publicKey);
+    const msgAccBalBefore = await getBal(messagePda);
 
     const updatedMsg = "Hello, Solana!";
-    await program.methods.update(updatedMsg).rpc({ commitment: "confirmed" });
+    const txn = await program.methods
+      .update(updatedMsg)
+      .rpc({ commitment: "confirmed" });
+    const networkFees = await getTxnFees(txn);
+
     const accountData = await program.account.messageAccount.fetch(
       messagePda,
       "confirmed"
     );
     expect(accountData.message).to.equal(updatedMsg);
 
-    const vaultBalAfter = await program.provider.connection.getBalance(
-      vaultPda
-    );
-    const userBalAfter = await program.provider.connection.getBalance(
-      wallet.publicKey
-    );
+    const vaultBalAfter = await getBal(vaultPda);
+    const userBalAfter = await getBal(wallet.publicKey);
+    const msgAccBalAfter = await getBal(messagePda);
+
     expect(vaultBalAfter).to.be.greaterThan(vaultBalBefore);
     expect(userBalAfter).to.be.lessThan(userBalBefore);
-    expect(userBalBefore - userBalAfter).to.be.greaterThan(
-      // greater than, cause network fees
-      vaultBalAfter - vaultBalBefore
+
+    const userSpent = userBalBefore - userBalAfter;
+    const msgAccExtraRent = msgAccBalAfter - msgAccBalBefore;
+    expect(userSpent).to.be.closeTo(
+      vaultBalAfter - vaultBalBefore + networkFees + msgAccExtraRent,
+      10
     );
   });
 
   it("Delete Message Account", async () => {
-    const userBalBefore = await program.provider.connection.getBalance(
-      wallet.publicKey
-    );
-    const vaultBalBefore = await program.provider.connection.getBalance(
-      vaultPda
-    );
-    const messageAccountBalBefore = (
+    const userBalBefore = await getBal(wallet.publicKey);
+    const vaultBalBefore = await getBal(vaultPda);
+    const messageAccBalBefore = (
       await program.provider.connection.getAccountInfo(messagePda)
     ).lamports;
 
-    await program.methods.delete().rpc({ commitment: "confirmed" });
+    const txn = await program.methods.delete().rpc({ commitment: "confirmed" });
     const accountData = await program.account.messageAccount.fetchNullable(
       messagePda,
       "confirmed"
     );
     expect(accountData).to.equal(null);
 
-    const userBalAfter = await program.provider.connection.getBalance(
-      wallet.publicKey
-    );
-    const vaultBalAfter = await program.provider.connection.getBalance(
-      vaultPda
-    );
+    const userBalAfter = await getBal(wallet.publicKey);
+    const vaultBalAfter = await getBal(vaultPda);
+    const networkFees = await getTxnFees(txn);
     expect(vaultBalAfter).to.equal(0);
-    expect(userBalAfter).to.be.lessThan(
-      userBalBefore + vaultBalBefore + messageAccountBalBefore
-    ); // less than cause network fees
+    expect(userBalAfter).to.be.closeTo(
+      userBalBefore + vaultBalBefore + messageAccBalBefore - networkFees,
+      100
+    );
   });
 });
